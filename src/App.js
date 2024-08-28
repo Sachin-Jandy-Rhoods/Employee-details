d926eb6dbe9d143b7d1b5b779e2d9d3d790a37b9
Sachin-Jandy-Rhoods <sachinjandyrhoods@example.com>
2024-08-28 22:59:01 +0530

    __

--- src/App.js
+++ src/App.js
@@ -5,15 +5,13 @@ import{Home} from "./component/Home"
 import { MonthlyReport } from './component/MonthlyReport';
 function App() {
   return (
-      <Router basename="/Employee-details">  {/* Replace <repo-name> with your repository name */}
       <Routes>
-        <Route path="/" element={<Home />}>
-          <Route path="employee" element={<EmployeData />} />
-          <Route path="monthlyreport/:name" element={<MonthlyReport />} />
-        </Route>
-        <Route path="*" element={<h1>Page Not Found</h1>} />
+            <Route path="/" element={<Home/>}>
+            <Route path='employee' element={<EmployeData/>}/>
+            <Route path="monthlyreport/:name" element={<MonthlyReport/>}/>
+            </Route>
+            <Route path='*' element={<h1>Page Not Fount</h1>}/>
       </Routes>
-    </Router>
   
 );
 }
